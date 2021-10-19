b9c35dfc23706a995e5087720bc271532ab7fadc
Artur <arturbabii08@gmail.com>
2021-10-19 23:23:37 +0300

    fix error add login

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -50,6 +50,48 @@ router.post(
             console.log(e);
         }
     });
+router.post(
+    '/login',
+    [
+        check('email', 'Email must be').normalizeEmail().isEmail(),
+        check('password', 'Must be exist').exists()
+    ],
+
+    async (request, response) => {
+        try {
+            const errors = validationResult(request);
+
+            if (!errors.isEmpty()) {
+                return response.status(400).json({
+                    errors: errors.array(),
+                    message: 'Some Wrong in login'
+                })
+            }
+            const { email, password } = request.body;
+
+            const user = await User.findOne({ email: email })
+
+            if (!user) {
+                return response.status(400).json({ message: 'User not found' });
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if(!isMatch){
+                return response.status(400).json({message: 'Password is wrong'});
+            }
+
+            const token = jwt.sign(
+                {userId: user.id},
+                config.get('jwtSecret'),
+                {expiresIn: '1h'}
+            )
+            response.json({token, userId: user.id})
+        } catch (e) {
+            response.status(500).json({ message: 'Some Error', e })
+            console.log(e);
+        }
+    });
 
 router.get(
     '/check',
