803a1aa31e9564e461eebf7fafa241b0944af67e
prosvitco-artur <arturbabii08@gmail.com>
2021-10-19 12:38:57 +0300

    add mongoDB

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const config = require('config');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const PORT = config.get('port') || 5000;
+
+async function start(){
+    try{
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            // useCreateIndex: true,
+        })
+    } catch(e){
+        console.log("Server Error", e.message);
+        process.exit(1);
+    }
+}
+
+
+app.listen(PORT, () => console.log('App startet on PORT ' + PORT));
+
+start();
