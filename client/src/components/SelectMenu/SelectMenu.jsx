0337d54ae5a7ab1385df0d5e343b3bf210d6cc5d
Artur <arturbabii08@gmail.com>
2021-10-25 21:51:07 +0300

    add scrolBar

--- client/src/components/SelectMenu/SelectMenu.jsx
+++ client/src/components/SelectMenu/SelectMenu.jsx
@@ -1,5 +1,5 @@
 
-import {ReactComponent as VectorLang} from '../../common/img/icon/vector_language.svg';
+import { ReactComponent as VectorLang } from '../../common/img/icon/vector_language.svg';
 
 const SelectMenu = (props) => {
     return (
@@ -8,15 +8,47 @@ const SelectMenu = (props) => {
                 {/*<img alt="car" src="img/icon/car.svg" />*/}
             </div>
             <div style={{ padding: '11px 0' }} className="float_left  size_14">Select vehicle</div>
-            <div className="float_left radius_50 background_grey select">
+            <div className="float_left radius_50 background_grey select ">
                 <span>Year</span>
                 <VectorLang />
-                {/*<ul className="box_shadow_4 background_white box_select">*/}
-                {/*    <li className="pointer_red">2021</li>*/}
-                {/*    <li className="pointer_red">2020</li>*/}
-                {/*    <li className="pointer_red">2019</li>*/}
-                {/*    <li className="pointer_red">2018</li>*/}
-                {/*</ul>*/}
+                <div className="box_shadow_4 background_white box_select scrolling">
+                    <p className="pointer_red">2021</p>
+                    <p className="pointer_red">2020</p>
+                    <p className="pointer_red">2019</p>
+                    <p className="pointer_red">2018</p>
+                    <p className="pointer_red">2018</p>
+                    <p className="pointer_red">2018</p>
+                    <p className="pointer_red">2018</p>
+                    <p className="pointer_red">2018</p>
+                    <p className="pointer_red">2018</p>
+                    <p className="pointer_red">2018</p>
+                    <p className="pointer_red">2018</p>
+                    <p className="pointer_red">2018</p>
+                    <p className="pointer_red">2021</p>
+                    <p className="pointer_red">2020</p>
+                    <p className="pointer_red">2019</p>
+                    <p className="pointer_red">2018</p>
+                    <p className="pointer_red">2018</p>
+                    <p className="pointer_red">2018</p>
+                    <p className="pointer_red">2018</p>
+                    <p className="pointer_red">2018</p>
+                    <p className="pointer_red">2018</p>
+                    <p className="pointer_red">2018</p>
+                    <p className="pointer_red">2018</p>
+                    <p className="pointer_red">2018</p>
+                    <p className="pointer_red">2021</p>
+                    <p className="pointer_red">2020</p>
+                    <p className="pointer_red">2019</p>
+                    <p className="pointer_red">2018</p>
+                    <p className="pointer_red">2018</p>
+                    <p className="pointer_red">2018</p>
+                    <p className="pointer_red">2018</p>
+                    <p className="pointer_red">2018</p>
+                    <p className="pointer_red">2018</p>
+                    <p className="pointer_red">2018</p>
+                    <p className="pointer_red">2018</p>
+                    <p className="pointer_red">2018</p>
+                </div>
             </div>
             <div className="float_left radius_50 background_grey select">
                 <span>Year</span>
