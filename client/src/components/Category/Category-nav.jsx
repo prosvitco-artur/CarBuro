146e4d857fc4829893728b94d4af55c1d0237e19
prosvitco-artur <arturbabii08@gmail.com>
2021-10-28 19:15:10 +0300

    add show category

--- client/src/components/Category/Category-nav.jsx
+++ client/src/components/Category/Category-nav.jsx
@@ -1,26 +1,44 @@
 import { connect } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { getProductCategory } from "../../redux/ProductReduser";
+import React, {useState} from "react";
 
 const CategoryNav = (props) => {
+
+    
+    let [category, setCategory] = useState();
+    let [showingCat, setShowingCat] = useState(false);
+
+    const showCurrentCategory = () =>{
+        if(!showingCat){
+            setCategory(props.categories.Interior);
+            setShowingCat(true);
+        } else {
+            setCategory();
+            setShowingCat(false);
+        } 
+
+    }
+        
+
+
     return (
         <div>
             <div className="container category_menu rubik_regular background_white">
-                {Object.keys(props.categories).map(key => <CategoryTitle key={key} name={key} />)}
+                {Object.keys(props.categories).map(key => <a onClick = {()=>showCurrentCategory()}><span className="pointer_red">{key}</span></a>)}
             </div>
+            {category && 
             <div className="container cat-list grid grid_columns_6 grid_gap_30_20 rubik_regular">
-                <div className="item">
-                    <img src="https://docs.google.com/presentation/d/1zZIJnYu-416-WB2HC27PtAa80-ha_LZh-48Oo-fK8eg/edit#slide=id.p1" alt={"Mirrors" + "test"} />
-                    <p>Mirrors</p>
-                </div>
+                {category.map(key=> <CategoryTitle key={key} name={key.name}  image={key.image} /> )}
             </div>
+            }
         </div>
     )
 }
 
 let CategoryTitle = (props) => {
     return (<NavLink to="/catalog">
-        <span className="pointer_red">{props.name}</span>
+        <div className="item"><img src={props.image} alt={"Mirrors" + "test"} /><p>{props.name}</p></div>
     </NavLink>)
 }
 let mapStateToProps = (state) => {
