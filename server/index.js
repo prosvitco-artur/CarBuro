9373ce8df6fa6644ed9a2be26146f6efa0e0a253
Artur <arturbabii08@gmail.com>
2021-11-02 22:38:50 +0300

    add static

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const fileUpload = require('express-fileupload');
 const router = require('./routes/index');
 const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+const path = require('path');
 
 const PORT = config.get("port") || 5000;
 
@@ -13,6 +14,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(fileUpload({}));
 app.use('/api', router);
 
