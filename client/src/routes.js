efdb16fa15c746d261cf6ae63732028bbd41b59b
prosvitco-artur <arturbabii08@gmail.com>
2021-11-04 17:29:08 +0200

    add pages

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,8 +1,14 @@
+import { ACCOUNT_ROUTE, ADMIN_ROUTE, CART_ROUTE, CATEGORY_ROUTE, CHECKOUT_ROUTE, HOME_ROUTE, LOGIN_ROUTE, PRODUCT_ROUTE } from "./utils/consts";
 import Catalog from "./components/Catalog/Catalog";
 import Product from "./pages/Product";
 import Admin from "./pages/Admin";
 import Home from "./pages/Home";
-import { ADMIN_ROUTE, CATEGORY_ROUTE, HOME_ROUTE, PRODUCT_ROUTE } from "./utils/consts";
+import Account from "./pages/Account";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Login from "./pages/Login";
+
+
 
 export const authRoutes = [
     {
@@ -22,5 +28,21 @@ export const publicRoutes = [
     {
         path: PRODUCT_ROUTE + '/:id',
         Component: Product
+    },
+    {
+        path: ACCOUNT_ROUTE,
+        Component: Account
+    },
+    {
+        path: CART_ROUTE,
+        Component: Cart
+    },
+    {
+        path: CHECKOUT_ROUTE,
+        Component: Checkout
+    },
+    {
+        path: LOGIN_ROUTE,
+        Component: Login
     },
 ];
--- client/src/utils/consts.js
+++ client/src/utils/consts.js
@@ -3,3 +3,6 @@ export const LOGIN_ROUTE = '/login';
 export const HOME_ROUTE = '/';
 export const CATEGORY_ROUTE = '/category';
 export const PRODUCT_ROUTE = '/product';
+export const ACCOUNT_ROUTE = '/account';
+export const CART_ROUTE = '/cart';
+export const CHECKOUT_ROUTE = '/checkout';
