10adb1e42a964893f885961461fda2ce4387eb10
Artur <arturbabii08@gmail.com>
2021-12-09 23:50:04 +0300

    fix category

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -5,25 +5,19 @@ const path = require('path');
 
 class CategoryController {
     async create(req, res) {
-            const { name, parentName, brandId } = req.body;
-
-            const parent = await Category.findOne({ where: { name: parentName } });
-            
-            let hierarchyLevel;
-            let parentId;
-
-            if (parent) {
-                hierarchyLevel = parent.hierarchyLevel + 1;
-                parentId = parent.id;
-            } else {
-                hierarchyLevel = 1;
-                parentId = null;
-            }
-            const { img } = req.files;
-            let fileName = uuid.v4() + ".jpg";
-            img.mv(path.resolve(__dirname, '..', 'static', fileName));
-            const category = await Category.create({ name, hierarchyLevel, parentId, img: fileName, brandId })
-            return res.json({ category });
+        const { name, parentId = null, brandId } = req.body;
+        let hierarchyLevel;
+        if (parentId) {
+            const parent = await Category.findOne({ where: { id: parentId } });
+            hierarchyLevel = parent.hierarchyLevel + 1;
+        } else {
+            hierarchyLevel = 1;
+        }
+        const { img } = req.files;
+        let fileName = uuid.v4() + ".jpg";
+        img.mv(path.resolve(__dirname, '..', 'static', fileName));
+        const category = await Category.create({ name, hierarchyLevel, parentId, img: fileName, brandId })
+        return res.json({ category });
     }
 
     async getAll(req, res) {
@@ -32,15 +26,15 @@ class CategoryController {
     }
     async getParent(req, res) {
         let category;
-        if(req.query.parentId){
+        if (req.query.parentId) {
             category = await Category.findAll({ where: { parentId: req.query.parentId } });
         } else {
             category = await Category.findAll({ where: { hierarchyLevel: 1 } });
         }
-        return res.json({category});
+        return res.json({ category });
     }
     async delete(req, res) {
-        const {name} = req.body;
+        const { name } = req.body;
         const category = await Category.destroy({ where: { name } });
         return res.json({ category });
     }
